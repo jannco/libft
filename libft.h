c5c7b9fd06218e8ae9e49c23b6068ced611845d1
Wikifix <116146281+wikifixcom@users.noreply.github.com>
2023-10-04 12:30:07 +0100

    add ft_memset.c, libft.h

---
+++ libft.h
@@ -0,0 +1,16 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}			t_list;
+
+
+
+#endif
