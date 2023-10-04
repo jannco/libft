21d5b73490247233345fc55cd105fec3794ac4cd
Wikifix <116146281+wikifixcom@users.noreply.github.com>
2023-10-04 19:31:09 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,15 +15,15 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	const unsigned char	*drc;
-	unsigned char *str;
+	const unsigned char	*s;
+	unsigned char *d;
 
 	i = 0;
-	drc = src;
-	str = dest;
+	s = src;
+	d = dest;
 	while (i < n)
 	{
-		str[i] = drc[i];
+		d[i] = s[i];
 		i++;
 	}
 	return (dest);
