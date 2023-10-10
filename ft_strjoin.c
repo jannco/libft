5257a7a9b3f77ad801ffe7b68d396f8a9cee866c
Yaroslav <wikifixcom@gmail.com>
2023-10-10 22:56:47 +0100

    done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -5,17 +5,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	size_t	i;
 
+	i = 0;
 	if (!s1 || !s2)
 		return (NULL);
 	str = (char *)malloc((strlen(s1) + strlen(s2) +1) * sizeof(char));
-	while (*str)
-	{
-
-	}
+	if (!str)
+		return (NULL);
+	while (*s1)
+		str[i++] = *(s1++);
+	while (*s2)
+		str[i++] = *(s2++);
+	str[i] = '\0';
 	return (str);
 }
-
-int main()
+/* 
+int	main(void)
 {
-	ft_strjoin("asd", "qwe");
-}
+	puts(ft_strjoin("asd", "qwe"));
+} */
